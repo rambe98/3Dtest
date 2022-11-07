2746098e4b21c0676332c1f145d062cad7c38e00
rambe98 <81672761+rambe98@users.noreply.github.com>
2022-11-08 00:07:58 +0900

    향상과제
    코드 올립니다

--- 3DProgramming-master/3DProgramming-master/3DProgramming/3DProgramming.cpp
+++ 3DProgramming-master/3DProgramming-master/3DProgramming/3DProgramming.cpp
@@ -1,78 +1,140 @@
-﻿#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
+﻿#include "MuSoenMath.h"
 
+void main() {
+    float x, y;
+    int answer;
+    cout << "X 좌표를 입력하세요." << endl;
+    cin >> x;
+    cout << "Y 좌표를 입력하세요." << endl;
+    cin >> y;
 
-#pragma comment(lib, "OpenGL32")
+    Vec3 Vector(x, y);
+    Mat3 Matrix = Mat3();
+    Vector.Print();
 
-static void error_callback(int error, const char* description)
-{
-    fputs(description, stderr);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-
-}
-
-
-int main(void)
-{
-    GLFWwindow* window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
-        exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    while (!glfwWindowShouldClose(window))
-    {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float)height;
-
-        float test = 0;
-
-        ////90도와 같이 각도로 연산하고 싶으면
-        ////glm의 삼각함수는 radian 형식으로 입력을 받기 때문에 변환해서 넣어주어야한다.
-        //glm::sin(glm::radians(90.0));  
-        
-        glClearColor(0.0f, 0.0f, 0.0f, 0.1f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_LINE_STRIP);
-
-        for (int i = 0; i <3600; i++) {
-
-            glVertex3f(glm::cos(glm::radians(45.0f)+ test), glm::sin(glm::radians(45.0f)+ test), 0);
-            test += 0.1f;
+    while (true) {
+        answer = 0;
+        cout << "원하는 기능의 번호를 입력하세요." << endl;
+        cout << "1. 이동" << endl;
+        cout << "2. 각도 조정" << endl;
+        cout << "3. 크기 조정" << endl;
+        cout << "4. 좌표 출력" << endl;
+        cout << "5. 종료" << endl;
+        cin >> answer;
+        switch (answer)
+        {
+        case 1:
+            cout << "X좌표를 이동시키고 싶은만큼 값을 입력하세요." << endl;
+            float moveX;
+            cin >> moveX;
+            cout << "Y좌표를 이동시키고 싶은만큼 값을 입력하세요." << endl;
+            float moveY;
+            cin >> moveY;
+            cout << "X좌표를 " << moveX << "만큼, Y좌표를 " << moveY << "만큼 이동합니다." << endl;
+            Matrix.MoveSet(moveX, moveY);
+            Vector = Vector * Matrix;
+            break;
+        case 2:
+            cout << "회전시킬 각도를 입력하세요." << endl;
+            float Rotation;
+            cin >> Rotation;
+            Matrix.RotateSet(Rotation);
+            Vector = Vector * Matrix;
+            break;
+        case 3:
+            cout << "몇 배로 할 것인지 값을 입력하세요." << endl;
+            float Size;
+            cin >> Size;
+            Matrix.ScaleSet(Size);
+            Vector = Vector * Matrix;
+            break;
+        case 4:
+            Vector.Print();
+            break;
+        case 5:
+            cout << "프로그램을 종료합니다." << endl;
+            return;
+        default:
+            break;
         }
-        
-        /*glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);
-        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-        glVertex3f(1.0f, -1.0f, 0.0f);
-        glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-        glVertex3f(-1.0f, -1.0f, 0.0f);
-        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-        glVertex3f(0.0f, 1.0f, 0.0f);*/
-        glEnd();   
-        glfwSwapBuffers(window);
-        glfwPollEvents();
     }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    
-    exit(EXIT_SUCCESS);
 }
+
+//#include <GLFW/glfw3.h>
+//#include <glm/glm.hpp>
+//
+//
+//#pragma comment(lib, "OpenGL32")
+//
+//static void error_callback(int error, const char* description)
+//{
+//    fputs(description, stderr);
+//}
+//static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+//{
+//    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+//        glfwSetWindowShouldClose(window, GL_TRUE);
+//
+//}
+//
+//
+//int main(void)
+//{
+//    GLFWwindow* window;
+//    glfwSetErrorCallback(error_callback);
+//    if (!glfwInit())
+//        exit(EXIT_FAILURE);
+//    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    glfwMakeContextCurrent(window);
+//    glfwSetKeyCallback(window, key_callback);
+//
+//    glEnable(GL_BLEND);
+//    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        float ratio;
+//        int width, height;
+//        glfwGetFramebufferSize(window, &width, &height);
+//        ratio = width / (float)height;
+//
+//        float test = 0;
+//
+//        ////90도와 같이 각도로 연산하고 싶으면
+//        ////glm의 삼각함수는 radian 형식으로 입력을 받기 때문에 변환해서 넣어주어야한다.
+//        //glm::sin(glm::radians(90.0));  
+//        
+//        glClearColor(0.0f, 0.0f, 0.0f, 0.1f);
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        glBegin(GL_LINE_STRIP);
+//
+//        for (int i = 0; i <3600; i++) {
+//
+//            glVertex3f(glm::cos(glm::radians(45.0f)+ test), glm::sin(glm::radians(45.0f)+ test), 0);
+//            test += 0.1f;
+//        }
+//        
+//        /*glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+//        glVertex3f(0.0f, 1.0f, 0.0f);
+//        glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+//        glVertex3f(1.0f, -1.0f, 0.0f);
+//        glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+//        glVertex3f(-1.0f, -1.0f, 0.0f);
+//        glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+//        glVertex3f(0.0f, 1.0f, 0.0f);*/
+//        glEnd();   
+//        glfwSwapBuffers(window);
+//        glfwPollEvents();
+//    }
+//    glfwDestroyWindow(window);
+//    glfwTerminate();
+//    
+//    exit(EXIT_SUCCESS);
+//}
